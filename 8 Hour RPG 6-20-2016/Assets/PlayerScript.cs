0f7715e9c2e900e2ed1459c46a709f6eb958239b
LoganPletcher <lpletch95@hotmail.com>
2016-06-20 13:55:08 -0500

    Initial Commit
    This is the basic starting idea for a project. It will not be actually
    used in the future project.

---
+++ 8 Hour RPG 6-20-2016/Assets/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.forward * 1 * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position += transform.right * 1 * Input.GetAxis("Horizontal") * Time.deltaTime;
+        float yAxis = transform.rotation.eulerAngles.y + (150.0F * Input.GetAxis("Mouse X") * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, yAxis, 0);
+    }
+}
---
+++ 8 Hour RPG 6-20-2016/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    Transform StartButton, HelpButton, ExitButton, HelpText, ReturnButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        foreach(Transform canvasitem in this.gameObject.GetComponentsInChildren<Transform>())
+        {
+            if (canvasitem.gameObject.name == "StartGameB")
+                StartButton = canvasitem;
+            else if (canvasitem.gameObject.name == "HtPButton")
+                HelpButton = canvasitem;
+            else if (canvasitem.gameObject.name == "ExitButton")
+                ExitButton = canvasitem;
+            else if (canvasitem.gameObject.name == "HowToPlayInstructions")
+                HelpText = canvasitem;
+            else if (canvasitem.gameObject.name == "ReturnButton")
+                ReturnButton = canvasitem;
+        }
+        StartButton.gameObject.SetActive(true);
+        HelpButton.gameObject.SetActive(true);
+        ExitButton.gameObject.SetActive(true);
+        HelpText.gameObject.SetActive(false);
+        ReturnButton.gameObject.SetActive(false);
+    }
+
+    public void StartGame(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void ShowInstructions()
+    {
+        StartButton.gameObject.SetActive(false);
+        HelpButton.gameObject.SetActive(false);
+        ExitButton.gameObject.SetActive(false);
+        HelpText.gameObject.SetActive(true);
+        ReturnButton.gameObject.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Return()
+    {
+        StartButton.gameObject.SetActive(true);
+        HelpButton.gameObject.SetActive(true);
+        ExitButton.gameObject.SetActive(true);
+        HelpText.gameObject.SetActive(false);
+        ReturnButton.gameObject.SetActive(false);
+    }
+}
